cc1ac17f4da60dfbaa86d1d6d2096e0be4e3591d
Adrian <colid.bondzio@gmail.com>
2022-03-03 17:49:24 +0100

    added line counter

--- ch1/main_test.go
+++ ch1/main_test.go
@@ -8,7 +8,17 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("w1 w2 w3 w4 w5 w6\n")
 	exp := 6
-	got := count(b)
+	got := count(b, false)
+
+	if got != exp {
+		t.Errorf("Expected %d, got %d.\n", exp, got)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("w1 w2 \nline2 \nline3 w3")
+	exp := 3
+	got := count(b, true)
 
 	if got != exp {
 		t.Errorf("Expected %d, got %d.\n", exp, got)
