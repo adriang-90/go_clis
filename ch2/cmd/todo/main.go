553aaff651afb8f96394ec546864d95c473c5bba
Adrian <colid.bondzio@gmail.com>
2022-03-09 22:59:12 +0100

    added flags to the program

--- ch2/cmd/todo/main.go
+++ ch2/cmd/todo/main.go
@@ -2,46 +2,63 @@ package main
 
 import (
 	"agasior90/go_clis/ch2/todo"
+	"flag"
 	"fmt"
 	"os"
-	"strings"
 )
 
-// Hardcoding the file name
 const todoFileName = ".todo.json"
 
 func main() {
+	// Parsing command line flags
+	task := flag.String("task", " ", "Task to be included in the ToDo List")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+
+	flag.Parse()
 
 	// Define an items list
 	l := &todo.List{}
 
-	// Use the Get method to read to do items from file
+	// Use the Get command to read to do items from file
 	if err := l.Get(todoFileName); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	// Decide what to do based on the number of arguments provided
+	// Decide what to do based on the provided flags
 	switch {
-	// For no extra arguments, print the list
-	case len(os.Args) == 1:
+	case *list:
 		// List current to do items
 		for _, item := range *l {
-			fmt.Println(item.Task)
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
+		}
+	case *complete > 0:
+		// Complete the given list
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
 
-	// Concatenate all prodived arguments with a space and
-	// add to the list as an item
-	default:
-		// Concatenate all arguments with a space
-		item := strings.Join(os.Args[1:], " ")
-		// Add the task
-		l.Add(item)
-
-		// Save the new list
+		// Save the new List
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+	case *task != "":
+		// Add the task
+		l.Add(*task)
+
+		//Save the new lList
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		// Invalid flag provided
+		fmt.Fprintln(os.Stderr, "Invalid option, pick another one")
+		os.Exit(1)
 	}
 }
